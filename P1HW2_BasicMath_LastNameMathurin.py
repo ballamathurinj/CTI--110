40e6d5b71587cb516b3c13b43e8cf5a92761cc00
ballamathurinj <71028275+ballamathurinj@users.noreply.github.com>
2020-09-15 12:20:14 -0400

    Add files via upload

---
+++ P1HW2_BasicMath_LastNameMathurin.py
@@ -0,0 +1,8 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=2
+>>> b=2
+>>> c=a*b
+>>> print("Mul=",c)
+Mul= 4
+>>> 
